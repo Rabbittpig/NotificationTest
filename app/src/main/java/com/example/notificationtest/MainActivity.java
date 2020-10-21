4e2579516b29a8d6eaf0c5af151065ea1437be91
zhangwanping <zhangwanping@tpson.cn>
2020-10-21 19:34:22 +0800

    Initial commit

---
+++ app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.notificationtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button sendNotice = findViewById(R.id.send_notice);
+        sendNotice.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.send_notice:
+                // 点击通知显示通知详情内容
+                Intent intent = new Intent(this,NotificationActivity.class);
+                PendingIntent pi = PendingIntent.getActivity(this,0,intent,0);
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                Notification notification = new NotificationCompat.Builder(this)
+                        .setContentTitle("This is content Title")
+                        .setContentText("This is content Text")
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
+                        .setContentIntent(pi)
+                        .setSound(Uri.fromFile(new File("/system/media/audio/ringtones/Luna.ogg"))) //音频
+                        .setVibrate(new long[]{0,1000,1000,1000})   //震动
+                        .setLights(Color.GREEN,1000,1000)  //led灯
+                        .setDefaults(NotificationCompat.DEFAULT_ALL) //设置默认效果
+                        .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(BitmapFactory.decodeResource(getResources(),R.drawable.img1)))
+                        .setPriority(NotificationCompat.PRIORITY_MAX)
+                        .build();
+                manager.notify(1, notification);
+                break;
+            default:
+                break;
+        }
+    }
+}
